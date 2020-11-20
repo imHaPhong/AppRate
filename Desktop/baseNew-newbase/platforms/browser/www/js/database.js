60b874f8b86a8971a7325e5c45738558a95ae166
LeVanTien126 <anivia2306@gmail.com>
2020-11-20 20:43:02 +0700

    appRate

---
+++ Desktop/baseNew-newbase/platforms/browser/www/js/database.js
@@ -0,0 +1,76 @@
+const listRes = [{
+        r_name: 'Restaurant 1',
+        r_type: 'Grill Food',
+        r_date: 'abc',
+        r_average_price: '300000',
+        r_service_rating: 2,
+        r_cleanliness_rating: 2,
+        r_food_rating: 2,
+        r_notes: ''
+    },
+    {
+        r_name: 'Restaurant 2',
+        r_type: 'See Food',
+        r_date: 'abc',
+        r_average_price: '300000',
+        r_service_rating: 5,
+        r_cleanliness_rating: 5,
+        r_food_rating: 5,
+        r_notes: ''
+    },
+    {
+        r_name: 'Restaurant 3',
+        r_type: 'Fast Food',
+        r_date: 'abc',
+        r_average_price: '300000',
+        r_service_rating: 3,
+        r_cleanliness_rating: 3,
+        r_food_rating: 3,
+        r_notes: ''
+    },
+    {
+        r_name: 'Restaurant 4',
+        r_type: 'Master',
+        r_date: 'abc',
+        average_price: '300000',
+        r_service_rating: 4,
+        r_cleanliness_rating: 4,
+        r_food_rating: 4,
+        r_notes: ''
+    }
+]
+
+var db;
+var request = window.indexedDB.open("Re-Data", 2);
+request.onupgradeneeded = function(event) {
+    var db = event.target.result;
+    var objectStore = db.createObjectStore("ReData", { keyPath: "id", autoIncrement: true });
+    for (var i in listRes) {
+        objectStore.add(listRes[i])
+    }
+}
+
+request.onsuccess = function(event) {
+    db = request.result;
+    console.log("Success: " + db);
+}
+
+function getAllData() {
+    const transaction = db.transaction(["ReData"], "readonly");
+    const objectStore = transaction.objectStore("ReData");
+    request = objectStore.getAll();
+    return request;
+}
+
+
+//delete
+function deleteFeedback(feedbackId) {
+    feedbackId = Number(feedbackId)
+    const deleteFb = db.transaction(["Re-Data"], "readwrite").objectStore("Re-Data").delete(feedbackId)
+    deleteFb.onsuccess = function() {
+        alert("delete successfully")
+        document.getElementById('list_data').innerHTML = ""
+        loadAllData()
+    }
+
+}
---
+++ Desktop/baseNew-newbase/platforms/browser/www/js/detail.js
@@ -0,0 +1,35 @@
+function loadAllData() {
+    let result = getAllData()
+    result.onsuccess = function(event) {
+        let data = event.target.result
+        console.log(data)
+        for (let i in data) {
+            let newContent = `<div class="col-md-4 text-center col-sm-6 col-xs-6">
+        <div class="product-box">
+            <div class="caption">
+                <h3>${data[i].r_name}</h3>
+                <p>${data[i].r_type}</p>
+                <p>Average Rating: <span>${parseFloat((Number(data[i].r_food_rating) + Number(data[i].r_cleanliness_rating) + Number(data[i].r_service_rating))/3).toFixed(1)}</p>
+                <button id="delete" feedbackId = "${data[i].id}" class="btn btn-danger">Delete</button>
+                <button id="detail" class="btn btn-primary" feedbackId = "${data[i].id}" type="button" data-toggle="modal" data-target="#detail1" >Information</button>
+            </div>
+        </div>
+        </div>`
+
+            $('#list_data').append(newContent)
+        }
+
+    }
+}
+
+$(window).on('load', function() {
+    loadAllData()
+})
+$(document).ready(function() {
+
+    $(document).on('click', '#delete', function() {
+        let id = $(this).attr('feedbackId')
+        deleteFeedback(id)
+    })
+
+})
